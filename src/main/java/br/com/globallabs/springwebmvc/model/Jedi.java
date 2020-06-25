2a679e85d1f9d5723b1ffc28fcedda1283d28e64
Alex Monteiro <alex.mecn@gmail.com>
2020-06-24 22:52:32 -0300

    First commit

---
+++ src/main/java/br/com/globallabs/springwebmvc/model/Jedi.java
@@ -0,0 +1,30 @@
+package br.com.globallabs.springwebmvc.model;
+
+public class Jedi {
+    private String name;
+    private String lastName;
+
+    public Jedi(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public Jedi() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
